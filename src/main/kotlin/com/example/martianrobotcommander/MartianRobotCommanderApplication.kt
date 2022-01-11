f69845b850da80ba3e13ed581c10f6ec99d4a14a
James <ja.whitfield@live.couk>
2022-01-11 20:39:29 +0000

    init empty project

---
+++ src/main/kotlin/com/example/martianrobotcommander/MartianRobotCommanderApplication.kt
@@ -0,0 +1,11 @@
+package com.example.martianrobotcommander
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MartianRobotCommanderApplication
+
+fun main(args: Array<String>) {
+    runApplication<MartianRobotCommanderApplication>(*args)
+}
